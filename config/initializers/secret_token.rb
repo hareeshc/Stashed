7b89ce8599037cc5a541ac2f3a07b57b7d2e6e68
Hareesh Chandrupatla <hareesh@anjinsolutions.com>
2012-02-06 20:22:23 -0500

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stashed::Application.config.secret_token = '66431f6e3af07bdab1994725600448e5a2726d1123674bb5c7a2ebc9d79aa3d2ad1e0eda78a3c75cb06c6dd3915fe43e0897294d0ef18bddd996c4022be011aa'
